11c577b0c1d698dd301b3222467a94c23a59503a
Pablo <Pablo.Iranzo@gmail.com>
2013-03-01 09:49:01 +0100

    Add smbios hook

---
+++ hooks/smbios/deploy.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# Description: Script for deploying scripts on hypervisor
+#
+# Author: Pablo Iranzo Gómez (Pablo.Iranzo@redhat.com)
+#
+
+HOST=$1
+if [ "$HOST" == "" ];
+then
+	echo "No host specified, exitting..."
+	exit 1
+fi
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+
+pushd .
+
+echo "Getting into script folder..."
+cd $DIR
+
+echo "Copying required files..."
+
+scp -q -o StrictHostKeyChecking=no -i /etc/pki/rhevm/keys/rhevm_id_rsa before_vm_start.py  $HOST:/usr/libexec/vdsm/hooks/before_vm_start/50_smbios
+
+echo "Changing permissions"
+ssh -q -o StrictHostKeyChecking=no  -i /etc/pki/rhevm/keys/rhevm_id_rsa $HOST persist /usr/libexec/vdsm/hooks/before_vm_start/50_smbios
+
+
+echo "Restarting VDSM to reflect changes"
+ssh -q -o StrictHostKeyChecking=no  -i /etc/pki/rhevm/keys/rhevm_id_rsa $HOST service vdsmd restart
+
+popd
