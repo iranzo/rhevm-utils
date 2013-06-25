6037bdf3e0262a83c83b06616a1c290f2c6c3a5d
Pablo <Pablo.Iranzo@gmail.com>
2013-06-26 00:44:46 +0200

    Fix usage help

--- rhev-vm-clone-to-export.py
+++ rhev-vm-clone-to-export.py
@@ -33,7 +33,7 @@ RHEV-vm-clone-to-export is a script for creating clones based from a vm snapshot
 """
 
 # Option parsing
-p = optparse.OptionParser("rhev-clone.py [arguments]", description=description)
+p = optparse.OptionParser("rhev-vm-clone-to-export.py [arguments]", description=description)
 p.add_option("-u", "--user", dest="username", help="Username to connect to RHEVM API", metavar="admin@internal", default="admin@internal")
 p.add_option("-w", "--password", dest="password", help="Password to use with username", metavar="admin", default="admin")
 p.add_option("-s", "--server", dest="server", help="RHEV-M server address/hostname to contact", metavar="127.0.0.1", default="127.0.0.1")
